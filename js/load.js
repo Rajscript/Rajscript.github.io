1d665e25e5b15ae33902b1cd8ceb61ed1eb10a4e
R D <rd@Rs-Laptop.local>
2023-12-13 11:28:15 -0500

    Modified the website

--- js/load.js
+++ js/load.js
@@ -1,13 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Logic for Blog Posts
-    var blogPosts = document.querySelectorAll('.blog-entry').length;
+    var blogEntries = document.querySelectorAll('.colorlib-blog .blog-entry'); // Define blogEntries here
     var blogLoadMoreButton = document.querySelector('.colorlib-blog .btn-load-more'); // Specific to blog section
+    
+    // Initially display only the first 3 blog entries
+    blogEntries.forEach((entry, index) => {
+        if (index > 2) {
+            entry.style.display = 'none'; // Hide extra entries
+        }
+    });
 
-    if (blogPosts > 3) {
+    if (blogEntries.length > 3) {
         blogLoadMoreButton.style.display = 'block';
     } else {
         blogLoadMoreButton.style.display = 'none';
     }
+
+    // Add event listener to the Load More button
+    blogLoadMoreButton.addEventListener('click', function() {
+        // Show all blog entries
+        blogEntries.forEach((entry) => {
+            entry.style.display = 'block';
+        });
+        // Optionally, hide the Load More button
+        blogLoadMoreButton.style.display = 'none';
+    });
+
 
     // Logic for Projects
     var projects = document.querySelectorAll('.project').length;
